bd0b8453d40f83f8d49d369731eccee9e19ce317
Adam Ibrahim <80495269+ninjakids27@users.noreply.github.com>
2025-09-13 22:54:59 -0400

    Add files via upload

---
+++ MatrixOps.java
@@ -0,0 +1,355 @@
+import java.util.Random;
+
+public class MatrixOps {
+    public static double[][] generate_Augmented_Matrix(int num_columns) {
+        Random random = new Random();
+        int num_rows = num_columns - 1;
+        double[][] Matrix_Name = new double[num_rows][num_columns];
+        for (int k = 0; k < num_rows; k++) {
+            for (int i = 0; i < num_columns; i++) {
+                Matrix_Name[k][i] = random.nextInt(10);
+            }
+        }
+        return Matrix_Name;
+    }
+    public static double[] generate_Random_vector(int length){
+        Random random = new Random();
+        double[] vector = new double[length];
+        for(int i = 0; i < length; i++){
+            vector[i] = random.nextInt(10);
+        }
+        return vector;
+    }
+    public static void Print_Matrix(double[][] Matrix_Name) {
+        int num_rows = Matrix_Name.length;
+        int num_columns = Matrix_Name[0].length;
+        for (int i = 0; i < num_rows; i++) {
+            for (int k = 0; k < num_columns; k++) {
+                System.out.print(Matrix_Name[i][k] + " ");
+            }
+            System.out.println("");
+        }
+    }
+    public static double[] access_row(double[][] Matrix, int row_index) {
+        int num_cols = Matrix[0].length;
+        double[] temp = new double[num_cols];
+        for (int i = 0; i < num_cols; i++) {
+            temp[i] = Matrix[row_index][i];
+        }
+        return temp;
+    }
+    public static double[] access_column(double[][] Matrix, int column_index) {
+        int num_rows = Matrix.length;
+        double[] temp = new double[num_rows];
+        for (int i = 0; i < num_rows; i++) {
+            temp[i] = Matrix[i][column_index];
+        }
+        return temp;
+    }
+    public static double[][] switch_rows(double[][] Matrix, int row1_index, int row2_index) {
+        // Clone the matrix
+        int numRows = Matrix.length;
+        int numCols = Matrix[0].length;
+        double[][] temp_Matrix = new double[numRows][numCols];
+        for (int i = 0; i < numRows; i++) {
+            for (int k = 0; k < numCols; k++) {
+                temp_Matrix[i][k] = Matrix[i][k];
+            }
+        }
+        double[] row1 = access_row(temp_Matrix, row1_index);
+        double[] row2 = access_row(temp_Matrix, row2_index);
+        for (int i = 0; i < row1.length; i++) {
+            temp_Matrix[row1_index][i] = row2[i];
+            temp_Matrix[row2_index][i] = row1[i];
+        }
+        return temp_Matrix;
+    }
+    public static double[][] replacement(double[][] matrix, int replacedRow, int rowAdded, double factor) {
+        double[] tempRowAdded = access_row(matrix, rowAdded);
+        int numRows = matrix.length;
+        int numCols = matrix[0].length;
+        double[][] tempMatrix = new double[numRows][numCols];
+        // Clone the matrix
+        for (int i = 0; i < numRows; i++) {
+            for (int k = 0; k < numCols; k++) {
+                tempMatrix[i][k] = matrix[i][k];
+            }
+        }
+        if (factor != 1) {
+            tempRowAdded = access_row(Constant_factor(matrix, rowAdded, factor), rowAdded);
+        }
+        for (int i = 0; i < tempRowAdded.length; i++) {
+            tempMatrix[replacedRow][i] = matrix[replacedRow][i] + tempRowAdded[i];
+        }
+        return tempMatrix;
+    }
+    public static double[][] Constant_factor(double[][] Matrix, int row_index, double factor) {
+        double[] temp = access_row(Matrix, row_index);
+        int numRows = Matrix.length;
+        int numCols = Matrix[0].length;
+        double[][] temp_Matrix = new double[numRows][numCols];
+        for (int i = 0; i < numRows; i++) {
+            for (int k = 0; k < numCols; k++) {
+                temp_Matrix[i][k] = Matrix[i][k];
+            }
+        }
+        for (int i = 0; i < temp.length; i++) {
+            temp_Matrix[row_index][i] = temp[i] * factor;
+        }
+        return temp_Matrix;
+    }
+    public static void Print_Vector(double[] Vector) {
+        for (int i = 0; i < Vector.length; i++) {
+            System.out.print(Vector[i] + " ");
+        }
+        System.out.println("");
+    }
+    public static void Print_Vector(int[] Vector) {
+        for (int i = 0; i < Vector.length; i++) {
+            System.out.print(Vector[i] + " ");
+        }
+        System.out.println("");
+    }
+    public static String getSolutionType(double[][] RREF_Matrix) {
+    int numRows = RREF_Matrix.length;
+    int numCols = RREF_Matrix[0].length;
+    int numVariables = numCols - 1; // Assuming it's an augmented matrix
+
+    // Check for no solution
+    for (int i = 0; i < numRows; i++) {
+        boolean allZerosExceptLast = true;
+        for (int j = 0; j < numVariables; j++) {
+            if (RREF_Matrix[i][j] != 0.0) {
+                allZerosExceptLast = false;
+                break;
+            }
+        }
+        if (allZerosExceptLast && RREF_Matrix[i][numVariables] != 0.0) {
+            return "No Solution";
+        }
+    }
+
+    // Check for free variables
+    int pivotCount = 0;
+    for (int i = 0; i < numRows; i++) {
+        for (int j = 0; j < numVariables; j++) {
+            if (RREF_Matrix[i][j] == 1.0) {
+                // Check if it's a leading 1
+                boolean isLeadingOne = true;
+                for (int k = 0; k < j; k++) {
+                    if (RREF_Matrix[i][k] != 0.0) {
+                        isLeadingOne = false;
+                        break;
+                    }
+                }
+                if (isLeadingOne) {
+                    pivotCount++;
+                    break;
+                }
+            }
+        }
+    }
+
+    if (pivotCount < numVariables) {
+        return "Infinitely Many Solutions (Free Variables)";
+    }
+
+    return "Unique Solution";
+    }  
+    public static double[][] RREF(double[][] Matrix, boolean debug) {
+    int numRows = Matrix.length;
+    int numCols = Matrix[0].length;
+    double[][] temp_Matrix = new double[numRows][numCols];
+
+    for (int i = 0; i < numRows; i++) {
+        for (int k = 0; k < numCols; k++) {
+            temp_Matrix[i][k] = Matrix[i][k];
+        }
+    }
+
+    double epsilon = 1e-9; 
+    int pivotRow = 0;
+
+    for (int j = 0; j < numCols && pivotRow < numRows; j++) {
+        int maxRow = pivotRow;
+        for (int i = pivotRow + 1; i < numRows; i++) {
+            if (Math.abs(temp_Matrix[i][j]) > Math.abs(temp_Matrix[maxRow][j])) {
+                maxRow = i;
+            }
+        }
+
+        if (Math.abs(temp_Matrix[maxRow][j]) > epsilon) { 
+            temp_Matrix = switch_rows(temp_Matrix, pivotRow, maxRow);
+            if(debug){
+                therefore();
+                Print_Matrix(temp_Matrix);
+            }
+
+            double pivotValue = temp_Matrix[pivotRow][j];
+            temp_Matrix = Constant_factor(temp_Matrix, pivotRow, 1/pivotValue);
+            temp_Matrix = roundMatrix(temp_Matrix, epsilon);
+            if(debug){
+                therefore();
+                Print_Matrix(temp_Matrix);
+            }
+
+            for(int i = pivotRow + 1; i < numRows; i++){
+                double factor = temp_Matrix[i][j];
+                temp_Matrix = replacement(temp_Matrix, i, pivotRow, -factor);
+                temp_Matrix = roundMatrix(temp_Matrix, epsilon);
+                if(debug){
+                    therefore();
+                    Print_Matrix(temp_Matrix);
+                }
+            }
+            pivotRow++;
+        }
+    }
+    
+    for(int i = numRows - 1; i >=0; i--){
+        int pivotCol = -1;
+        for(int j = 0; j < numCols; j++){
+            if(Math.abs(temp_Matrix[i][j] - 1.0) < epsilon){ 
+                pivotCol = j;
+                break;
+            }
+        }
+        if(pivotCol != -1){
+            for(int k = 0; k < i; k++){
+                double factor = temp_Matrix[k][pivotCol];
+                temp_Matrix = replacement(temp_Matrix, k, i, -factor);
+                temp_Matrix = roundMatrix(temp_Matrix, epsilon);
+                if(debug){
+                    therefore();
+                    Print_Matrix(temp_Matrix);
+                }
+            }
+        }
+    }
+    
+    temp_Matrix = roundMatrix(temp_Matrix, epsilon);
+    if(debug){
+        therefore();
+    }
+    return temp_Matrix;
+}
+    public static double[][] RREF(double[][] Matrix) {
+    int numRows = Matrix.length;
+    int numCols = Matrix[0].length;
+    double[][] temp_Matrix = new double[numRows][numCols];
+
+    for (int i = 0; i < numRows; i++) {
+        for (int k = 0; k < numCols; k++) {
+            temp_Matrix[i][k] = Matrix[i][k];
+        }
+    }
+
+    double epsilon = 1e-9; 
+    int pivotRow = 0;
+
+    for (int j = 0; j < numCols && pivotRow < numRows; j++) {
+        int maxRow = pivotRow;
+        for (int i = pivotRow + 1; i < numRows; i++) {
+            if (Math.abs(temp_Matrix[i][j]) > Math.abs(temp_Matrix[maxRow][j])) {
+                maxRow = i;
+            }
+        }
+
+        if (Math.abs(temp_Matrix[maxRow][j]) > epsilon) { 
+            temp_Matrix = switch_rows(temp_Matrix, pivotRow, maxRow);
+
+            double pivotValue = temp_Matrix[pivotRow][j];
+            temp_Matrix = Constant_factor(temp_Matrix, pivotRow, 1/pivotValue);
+            temp_Matrix = roundMatrix(temp_Matrix, epsilon);
+
+            for(int i = pivotRow + 1; i < numRows; i++){
+                double factor = temp_Matrix[i][j];
+                temp_Matrix = replacement(temp_Matrix, i, pivotRow, -factor);
+                temp_Matrix = roundMatrix(temp_Matrix, epsilon);
+            }
+            pivotRow++;
+        }
+    }
+    
+    for(int i = numRows - 1; i >=0; i--){
+        int pivotCol = -1;
+        for(int j = 0; j < numCols; j++){
+            if(Math.abs(temp_Matrix[i][j] - 1.0) < epsilon){ 
+                pivotCol = j;
+                break;
+            }
+        }
+        if(pivotCol != -1){
+            for(int k = 0; k < i; k++){
+                double factor = temp_Matrix[k][pivotCol];
+                temp_Matrix = replacement(temp_Matrix, k, i, -factor);
+                temp_Matrix = roundMatrix(temp_Matrix, epsilon);
+            }
+        }
+    }
+    
+    temp_Matrix = roundMatrix(temp_Matrix, epsilon);
+    return temp_Matrix;
+}
+    private static double[][] roundMatrix(double[][] matrix, double epsilon) {
+    int numRows = matrix.length;
+    int numCols = matrix[0].length;
+    for (int i = 0; i < numRows; i++) {
+        for (int j = 0; j < numCols; j++) {
+            if (Math.abs(matrix[i][j]) < epsilon) {
+                matrix[i][j] = 0.0;
+            }
+        }
+    }
+    return matrix;
+}
+    public static void therefore() {
+        System.out.println("->");
+    }
+    public static double dot_product(double[] vector1,double[] vector2, boolean debug){
+        double temp = 0;
+        if(vector1.length == vector2.length){
+            for(int i = 0; i < vector1.length; i++){
+                temp += vector1[i]*vector2[i];
+                if(debug){
+                    System.out.println(vector1[i]*vector2[i]);
+                }
+            }
+            return temp;
+        }else{
+            throw new IllegalArgumentException("John fucked up the lengths");
+        }
+    }
+    public static double[][] generate_Matrix(int Rows, int Columns){
+        double[][] Matrix = new double[Rows][Columns];
+        Random random = new Random();
+        for(int i = 0; i < Rows; i++){
+            for(int k = 0; k < Columns; k++){
+                Matrix[i][k] = random.nextInt(10);
+            }
+        }
+        return Matrix;
+
+    }
+    public static double[][] Matrix_Mult(double[][] Matrix_A, double[][] Matrix_B, boolean debug){
+        if(Matrix_A[0].length == Matrix_B.length){
+            // proceed as usual type shit
+            double[][] Result_Matrix = new double[Matrix_A.length][Matrix_B[0].length];
+            for(int i = 0; i < Matrix_A.length; i++){
+                for(int j = 0; j < Matrix_B[0].length; j++){
+                    double[] Row = access_row(Matrix_A, i);
+                    double[] Column = access_column(Matrix_B, j);
+                    Result_Matrix[i][j] = dot_product(Row, Column, false);
+                }
+            }
+            return Result_Matrix;
+        } else{
+            throw new IllegalArgumentException("John is *confirmed* to be gay");
+
+        }
+    }
+    // TO DO: understand how this shit works...
+    public static int cross_product(double[] vector1, double[] vector2){
+
+    }
+}
---
+++ StatOps.java
@@ -0,0 +1,275 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+public class StatOps {
+    public static double mean(double[] vector){
+        double temp = sum(vector);
+        temp /= vector.length;
+        return temp;
+    }
+    public static double sum(double[] array){
+        double sum = 0;
+        for(int i = 0; i < array.length; i++){
+            sum += array[i];
+        }
+        return sum;
+    }
+    public static int  sumD(double[] array){
+        int sum = 0;
+        for(int i = 0; i < array.length; i++){
+            sum += array[i];
+        }
+        return sum;
+    }
+    public static double weighted_mean(double[] vector, double[] weights,boolean debug){
+        double A = MatrixOps.dot_product(vector, weights,debug);
+        double B = sum(weights);
+        double C = A/B;
+        return C;
+    }
+    // Standard bubblesort
+    public static double[] sort(double[] arr) {
+        // Create a new array to avoid modifying the original
+        double[] sortedArr = Arrays.copyOf(arr, arr.length);
+
+        int n = sortedArr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (sortedArr[j] > sortedArr[j + 1]) {
+                    // Swap sortedArr[j] and sortedArr[j+1]
+                    double temp = sortedArr[j];
+                    sortedArr[j] = sortedArr[j + 1];
+                    sortedArr[j + 1] = temp;
+                }
+            }
+        }
+        return sortedArr;
+    }
+    public static double median(double[] vector){
+        double[] sort_list = sort(vector);
+        if(sort_list.length % 2 == 0){
+            int a = sort_list.length/2;
+            int b = sort_list.length/2-1;
+            double[] input = {sort_list[a],sort_list[b]};
+            return mean(input);
+        }else{
+            int a = sort_list.length/2;
+            return sort_list[a];
+        }
+    }
+    public static double[] vectorAdd(double[] vector, double factor){
+        for(int i = 0; i<vector.length; i++){
+            vector[i] += factor;
+        }
+        return vector;
+    }
+    public static double[] vectorMult(double[] vector, double factor){
+        for(int i = 0; i<vector.length; i++){
+            vector[i] *= factor;
+        }
+        return vector;
+    }
+    public static double[] vectorPow(double[] vector, double factor){
+        for(int i = 0; i<vector.length; i++){
+            vector[i] = Math.pow(vector[i], factor);
+        }
+        return vector;
+    }
+    public static int[] findMax(double[] vector){
+        double Max = vector[0];
+        List<Integer> indexes = new ArrayList<>();
+        for(int i = 0; i < vector.length; i++){
+            if(Max < vector[i]){
+                Max = vector[i];
+                indexes.clear();
+                indexes.add(i);
+            }else if(Max == vector[i]){
+                indexes.add(i);
+            }
+        }
+        int[] return_indexes = new int[indexes.size()];
+        for(int i = 0; i < return_indexes.length;i++){
+            return_indexes[i] = indexes.get(i);
+        }
+        return return_indexes;
+
+    }
+    public static int[] findMax(int[] vector){
+        double Max = vector[0];
+        List<Integer> indexes = new ArrayList<>();
+        for(int i = 0; i < vector.length; i++){
+            if(Max < vector[i]){
+                Max = vector[i];
+                indexes.clear();
+                indexes.add(i);
+            }else if(Max == vector[i]){
+                indexes.add(i);
+            }
+        }
+        int[] return_indexes = new int[indexes.size()];
+        for(int i = 0; i < return_indexes.length;i++){
+            return_indexes[i] = indexes.get(i);
+        }
+        return return_indexes;
+
+    }
+    public static double[] mode(double[] vector){
+        // I decided to kill this shit before it got WAYYY to out of hand
+        List<Double> item = new ArrayList<>();
+        List<Integer> amount = new ArrayList<>();
+        // could do a 2d array but I'm honestly too lazy for that shit
+
+        for(int i = 0; i < vector.length; i++){
+            if(item.contains(vector[i])){
+                amount.set(item.indexOf(vector[i]), amount.get(item.indexOf(vector[i]))+1);
+            }else{
+                item.add(vector[i]);
+                amount.add(1);
+            };
+        }
+        int[] amount_caveman = new int[amount.size()];
+        for(int i = 0; i < amount.size(); i++){
+            amount_caveman[i] = amount.get(i);
+        }
+        int[] a = findMax(amount_caveman);
+        double[] b = new double[a.length];
+        for(int i = 0; i < a.length;i++){
+            b[i] = item.get(a[i]);
+        }
+        return b;
+    }
+    public static double[] mode(int[] vector){
+        // I decided to kill this shit before it got WAYYY to out of hand
+        List<Integer> item = new ArrayList<>();
+        List<Integer> amount = new ArrayList<>();
+        // could do a 2d array but I'm honestly too lazy for that shit
+
+        for(int i = 0; i < vector.length; i++){
+            if(item.contains(vector[i])){
+                amount.set(item.indexOf(vector[i]), amount.get(item.indexOf(vector[i]))+1);
+            }else{
+                item.add(vector[i]);
+                amount.add(1);
+            };
+        }
+        int[] amount_caveman = new int[amount.size()];
+        for(int i = 0; i < amount.size(); i++){
+            amount_caveman[i] = amount.get(i);
+        }
+        int[] a = findMax(amount_caveman);
+        double[] b = new double[a.length];
+        for(int i = 0; i < a.length;i++){
+            b[i] = item.get(a[i]);
+        }
+        return b;
+    }
+    public static double midRange(double[] vector){
+        double[] a = sort(vector);
+        return (a[0]+a[a.length])/2;
+    }
+    public static double range(double[] vector){
+        double[] a = sort(vector);
+        return (a[a.length]-a[0]);
+    }
+    public static double getQ1(double vector[]){
+        double[] data = sort(vector);
+        if(data.length+1 % 4 == 0){
+            return data[(data.length+1)/4];
+        }else{
+            int n1 = (int) (data.length+1.5)/4;
+            int n2 = (int) (data.length+0.5)/4;
+            double[] b = {vector[n1],vector[n2]};
+            double a = mean(b);
+            return a;
+        }
+    }
+    public static double getQ3(double vector[]){
+        double[] data = sort(vector);
+        if((3*(data.length+1)) % 4 == 0){
+            return data[(3*(data.length+1))/4 -1];
+        }else{
+            int n1 = (int) ((3*(data.length+1)+0.5)/4);
+            int n2 = (int) ((3*(data.length+1)-0.5)/4);
+            double[] b = {data[n1-1],data[n2-1]};
+            double a = mean(b);
+            return a;
+        }
+        
+    }
+    public static void five_number_summary_of_freq(double[] vector1, double[] vector2){
+        List<Double> table = new ArrayList<>();
+        for(int i = 0; i < vector1.length; i++){
+            for(int k = 0; k < (int)vector2[i]; k++){
+                table.add(vector1[i]);
+            }
+        }
+        
+        double[] table_double = new double[table.size()];
+        for (int i = 0; i < table.size(); i++) {
+            table_double[i] = table.get(i);
+        }
+        double[] D = sort(table_double);
+        System.out.println("Min: "+D[0]);
+        System.out.println("Q1: "+getQ1(D));
+        System.out.println("Median: "+median(D));
+        System.out.println("Q3: "+getQ3(D));
+        System.out.println("Max: "+D[D.length-1]);
+    }
+    public static double rounding(double num, int decimal){
+        return Math.round(Math.pow(10, decimal)*num)/Math.pow(10, decimal);
+    }
+    public static void outlier_test(double[] vector){
+        
+    }
+    public static double stdDeviation(double[] vector){
+        double mean = mean(vector);
+        vector = vectorAdd(Arrays.copyOf(vector, vector.length), -mean);
+        vector = vectorPow(vector, 2);
+        double val = sum(vector);
+        return Math.sqrt(val/(vector.length));
+    }
+    public static double samDeviation(double[] vector){
+        double mean = mean(vector);
+        vector = vectorAdd(Arrays.copyOf(vector, vector.length), -mean);
+        vector = vectorPow(vector, 2);
+        double val = sum(vector);
+        return Math.sqrt(val/(vector.length-1));
+    }
+    public static double samVariance(double[] vector){
+        double mean = mean(vector);
+        vector = vectorAdd(Arrays.copyOf(vector, vector.length), -mean);
+        vector = vectorPow(vector, 2);
+        double val = sum(vector);
+        return val/(vector.length-1);
+        
+    }
+    public static double popVariance(double[] vector){
+        double mean = mean(vector);
+        vector = vectorAdd(Arrays.copyOf(vector, vector.length), -mean);
+        vector = vectorPow(vector, 2);
+        double val = sum(vector);
+        return val/(vector.length);
+        
+    }
+    public static void OneVarStat(double[] vector){
+        
+    }
+    public static double Ztest(double observedVal,double[] vector,boolean isSample){
+        double z;
+        if(isSample){
+           z = (observedVal-mean(vector))/samDeviation(vector); 
+        }else{
+            z = (observedVal-mean(vector))/stdDeviation(vector);
+        }
+        return z;
+    }
+    public static double Ztest(double observedVal,double mean, double stdDeviation){
+           double z = (observedVal-mean)/stdDeviation;
+           return z;
+    }
+    public static void five_number_summary(double[] vector){
+    
+    }
+    
+    
+}
