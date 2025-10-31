0fb437df3e551bf336c8635bdad4ea936407e49f
Adam Ibrahim <adamivrahim@gmail.com>
2025-10-30 21:44:44 -0400

    reorganized

--- physicsOps.java
+++ physicsOps.java
@@ -1,10 +1,26 @@
 public class physicsOps{
-    // lazy physics functions :/
-    private static final double G = 6.674e-11;
+    private static final double G = 100;
 
+  public static double velocity(double mass1, double radius){
+    return (G*Math.sqrt(mass1/radius));
+  }
 
-    
-    public static void lazy(double mass1, double mass2, double radius){
-        
-    }
+  public static double period(double mass1, double radius){
+    return (2*Math.PI*radius)/velocity(mass1, radius);
+  }
+
+  public static double centripitalAcceleration(double m1, double radius){
+    return (Math.pow(velocity(m1, radius),2)/radius);
+  }
+
+  public static double gravitationalForce(double m1, double m2, double radius){
+    return (m2*centripitalAcceleration(m1, radius));
+  }
+
+  public static void summary(double mass1, double mass2, double radius){
+    System.out.println(ColorText.dataFormat("Velocity: " + velocity(mass1, radius)));
+    System.out.println(ColorText.dataFormat("Period: " + period(mass1, radius)));
+    System.out.println(ColorText.dataFormat("Centripital Acceleration: " + centripitalAcceleration(mass1, radius)));
+    System.out.println(ColorText.dataFormat("Gravitational Force: " + gravitationalForce(mass1, mass2, radius)));
+  }
 }
